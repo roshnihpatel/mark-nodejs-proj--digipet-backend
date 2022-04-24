a36a5746d7a67dc93e34bed31e53d30c77b9d0ef
Roshni Patel <roshnihpatel@outlook.com>
2022-04-24 11:10:56 +0100

    added feeding and training endpoints to server and controller functions, all traning and feeing unit and integration test are passing

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import { getDigipet } from "./digipet/model";
-import { hatchDigipet, walkDigipet } from "./digipet/controller";
+import { hatchDigipet, walkDigipet, trainDigipet, feedDigipet} from "./digipet/controller";
 
 const app = express();
 
@@ -68,6 +68,37 @@ app.get("/digipet/walk", (req, res) => {
     res.json({
       message:
         "You don't have a digipet to walk! Try hatching one with /digipet/hatch",
+    });
+  }
+});
+
+app.get("/digipet/train", (req, res) => {
+  // check the user has a digipet to walk
+  if (getDigipet()) {
+    trainDigipet();
+    res.json({
+      message: "You trained your digipet. It is more disicplined",
+      digipet: getDigipet(),
+    });
+  } else {
+    res.json({
+      message:
+        "You don't have a digipet to train! Try hatching one with /digipet/hatch",
+    });
+  }
+});
+app.get("/digipet/feed", (req, res) => {
+  // check the user has a digipet to walk
+  if (getDigipet()) {
+    feedDigipet();
+    res.json({
+      message: "You fed your digipet",
+      digipet: getDigipet(),
+    });
+  } else {
+    res.json({
+      message:
+        "You don't have a digipet to feed! Try hatching one with /digipet/hatch",
     });
   }
 });
